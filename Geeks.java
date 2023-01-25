14792cca033e0f7b635254dccbb12f5ae9299a2e
Ibzz7 <112071393+Ibzz7@users.noreply.github.com>
2023-01-25 23:03:30 +0530

    Add files via upload

---
+++ Geeks.java
@@ -0,0 +1,13 @@
+	public static void main(String[] args)
+	{
+                       String str1 = "Geeksforgeeks";
+		System.out.println(isValidUsername(str1));
+
+		
+		String str3 = "1Geeksforgeeks";
+		System.out.println(isValidUsername(str3));
+
+		String str5 = "Ge";
+		System.out.println(isValidUsername(str5));
+	}
+}
